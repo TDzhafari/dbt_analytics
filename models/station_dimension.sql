66cd5185e76b2d20c2c85f9847f6150517059111
Timur Dzhafari <gagarintim01@gmail.com>
2026-04-20 00:56:18 +0000

    added: station_dimension model

---
+++ models/station_dimension.sql
@@ -0,0 +1,14 @@
+WITH BIKE AS (
+    SELECT 
+        DISTINCT
+
+        START_STATION_ID as station_id,
+        START_STATION_NAME as station_name,        
+        START_LAT as station_lat,
+        START_LNG as start_station_lng
+
+    FROM {{ source('DEMO', 'BIKE') }}
+    WHERE RIDE_ID != 'ride_id'
+    LIMIT 10
+)
+SELECT * FROM BIKE
